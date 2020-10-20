cf0ff7cb59facc4158f43092c50c7ccc47948b28
Victor A. Colina L <victorcolina2202@gmail.com>
2020-10-20 17:24:59 -0300

    Conditional Profile Card

--- src/js/index.js
+++ src/js/index.js
@@ -28,19 +28,31 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  if (variables.name == null) variables.name = "Lucy";
+  if (variables.lastname == null) variables.lastname = "Boilett";
+  if (variables.role == null) variables.role = "Web Developer";
+  if (variables.city == null) variables.city = "Miami";
+  if (variables.country == null) variables.country = "USA";
+  if (variables.twitter == null) variables.twitter = "Colinaleon22";
+  if (variables.github == null) variables.github = "victorcolina22";
+  if (variables.linkedin == null)
+    variables.linkedin = "victor-colina-leon-8a38541b8";
+  if (variables.instagram == null) variables.instagram = "victoracl22";
+  if (variables.socialMediaPosition == null)
+    variables.socialMediaPosition = "left";
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
